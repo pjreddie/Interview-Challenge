b8873fa8afb78efa5419b286eb33d680cd80d9c8
Joe Redmon <pjreddie@unknown20c9d046e7e1.att.net>
2012-10-25 16:31:08 -0700

    One possible solution to the challenge

--- challenge.py
+++ challenge.py
@@ -1,5 +1,25 @@
+def wordlist():
+    words_file = open("words.txt")
+    words = set()
+    for line in words_file:
+        words.add(line.strip().lower())
+    return words
+
+words = wordlist()
+
 def split(s):
-    return s
+    s = s.lower()
+    s = s.replace(" ", "")
+    if s in words:
+        return [s]
+    else:
+        for i in range(1,len(s)):
+            if s[:i] in words:
+                other = split(s[i:])
+                if other:
+                    return [s[:i]]+other
+    return None
+
 
 def main():
     in_file = open("split.txt")
